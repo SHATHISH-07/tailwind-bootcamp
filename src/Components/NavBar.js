c6f6f98df2eb04ce8f6a1197730bd5b48a0361ec
SHATHISH <satheesh12072004@gmail.com>
2024-09-28 11:55:04 +0530

    added the hamburger menu to the navBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 import Logo from "../img/logo.svg";
 
 const NavBar = () => {
+  const [isOpen, setIsOPen] = useState("false");
+
+  function toggleMenu() {
+    setIsOPen(!isOpen);
+  }
+
   return (
     <nav className="relative container mx-auto p-6">
       {/* <!-- Flex container --> */}
@@ -40,6 +46,33 @@ const NavBar = () => {
         >
           Get Started
         </a>
+
+        {/* Hamburger Icon */}
+
+        <button
+          id="menu-btn"
+          className={`block hamburger md:hidden focus:outline-none ${
+            isOpen ? "open" : ""
+          }`}
+          onClick={toggleMenu}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      {/* Mobile menu */}
+      <div className={`md:hidden ${isOpen ? "block" : "hidden"}`}>
+        <div
+          id="menu"
+          className=" absolute  flex flex-col items-center self-end  py-8 mt-10 space-y-6 font-bold bg-white drop-shadow-md sm:w-auto sm:self-center left-6 right-6 "
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Products</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
       </div>
     </nav>
   );
