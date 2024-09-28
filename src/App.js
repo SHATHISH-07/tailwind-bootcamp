f3d09bd87b6a8a73249dfd13fda6d0cdab074ca8
SHATHISH <satheesh12072004@gmail.com>
2024-09-28 10:21:41 +0530

    Added the CTA section to the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import CTAsection from "./Components/CTAsection";
 import FeatureSec from "./Components/FeatureSec";
 import HeroSection from "./Components/HeroSection";
 import NavBar from "./Components/NavBar";
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <FeatureSec />
       <Testimonials />
+      <CTAsection />
     </>
   );
 }
